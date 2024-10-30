d96eea62ff9f0244995475f767d0c3314a7307a7
leo.yao <LEO.YAO@TECKY.IO>
2024-10-30 14:57:15 +0800

    fix squatter & dlo

--- setup.sql
+++ setup.sql
@@ -31,16 +31,34 @@ CREATE OR REPLACE PROCEDURE find_dlo_id(
     p_dlooffice IN VARCHAR2,
     p_dlo_id OUT VARCHAR2
 ) IS
+    v_dlo_ids VARCHAR2(4000);
 BEGIN
-    SELECT ID
-    INTO p_dlo_id
-    FROM SC.DLOS dlos
-    WHERE DLO_NAME LIKE '%' || p_dlooffice || '%';
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        p_dlo_id := NULL;
-    WHEN TOO_MANY_ROWS THEN
-        p_dlo_id := NULL;
-        -- Log an error if needed, as this indicates a data issue
-        log_error('DLO', 'Multiple records found for DLOOFFICE: ' || p_dlooffice);
+    BEGIN
+        -- Attempt to find a single ID based on the DLO_NAME
+        SELECT ID
+        INTO p_dlo_id
+        FROM SC.DLOS dlos
+        WHERE DLO_NAME LIKE '%' || '/' || p_dlooffice || '%';
+            OR (p_dlooffice = 'N' AND DLO_NAME LIKE '%/ND%');
+            OR (p_dlooffice = 'HKWS' AND DLO_NAME LIKE '%/HKW&S%');
+            OR (p_dlooffice = 'KT' AND DLO_NAME LIKE '%/TW&KT%');
+
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            p_dlo_id := NULL;
+            log_error('DLO', 'NO FOUND DLOOFFICE: ' || p_dlooffice);
+
+        WHEN TOO_MANY_ROWS THEN
+            p_dlo_id := NULL;
+            
+            -- Collect all IDs that match the condition to log them
+            SELECT LISTAGG(ID, ', ') WITHIN GROUP (ORDER BY ID)
+            INTO v_dlo_ids
+            FROM SC.DLOS
+            WHERE DLO_NAME LIKE '%' || p_dlooffice || '%';
+            
+            -- Log an error with the concatenated list of IDs for debugging
+            log_error('DLO', 'Multiple records found for DLOOFFICE: ' || p_dlooffice || 
+                             '. Found IDs: ' || v_dlo_ids);
+    END;
 END;
