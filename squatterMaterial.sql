9d4f65133ba62ad2645b932fec8d34ab90857851
leo.yao <LEO.YAO@TECKY.IO>
2024-11-06 16:52:15 +0800

    fix sm

--- squatterMaterial.sql
+++ squatterMaterial.sql
@@ -15,31 +15,58 @@ BEGIN
             sm.last_edited_date,
             sm.VERSION,
             m.ID AS MATERIAL_ID,
-            s.ID AS SQUATTER_GUID
+            s.ID AS SQUATTER_GUID,
+            sp.ID AS SQUATTER_PEND_GUID
         FROM SDE_SQ.SQUATTERMATERIAL sm
-        LEFT JOIN SQ.MATERIALS m ON m.NAME = sm.MATERIALS
-        LEFT JOIN SQ.SQUATTERS s ON s.SQUATTER_ID = sm.SQUATTERID
+        LEFT JOIN SQ.MATERIALS m ON sm.MATERIALS = m.NAME 
+        LEFT JOIN SQ.SQUATTERS s ON sm.SQUATTERID = s.SQUATTER_ID AND sm.VERSION = s.VERSION
+        LEFT JOIN SQ.SQUATTER_PEND sp ON sm.SQUATTERID = sp.SQUATTER_ID AND sm.VERSION = sp.VERSION
     ) LOOP
         BEGIN
             generate_Formatted_GUID(v_guid);
-            -- Insert into the new SQUATTER_MATERIAL table
-            INSERT INTO SQ.SQUATTER_MATERIALS (
-                ID,
-                SQUATTER_ID, 
-                MATERIAL_ID, 
-                SQUATTER_GUID, 
-                SQUATTER_VERSION,
-                CREATED_AT,
-                UPDATED_AT
-            ) VALUES (
-                v_guid,
-                rec.SQUATTERID, 
-                rec.MATERIAL_ID,
-                rec.SQUATTER_GUID,
-                rec.VERSION,
-                rec.created_date,
-                rec.last_edited_date
-            );
+            IF rec.SQUATTER_GUID IS NULL AND rec.SQUATTER_PEND_GUID IS NULL THEN
+                v_error_message := 'Missing mandatory join data: SQUATTER_PEND_GUID AND SQUATTER_GUID is NULL!!';
+                log_error('SQUATTER_MATERIAL', v_error_message, rec.OBJECTID);
+            ELSEIF rec.SQUATTER_GUID IS NOT NULL AND rec.SQUATTER_PEND_GUID IS NULL THEN
+                INSERT INTO SQ.SQUATTER_MATERIALS (
+                    ID,
+                    SQUATTER_ID, 
+                    MATERIAL_ID, 
+                    SQUATTER_GUID, 
+                    SQUATTER_VERSION,
+                    CREATED_AT,
+                    UPDATED_AT
+                ) VALUES (
+                    v_guid,
+                    rec.SQUATTERID, 
+                    rec.MATERIAL_ID,
+                    rec.SQUATTER_GUID,
+                    rec.VERSION,
+                    rec.created_date,
+                    rec.last_edited_date
+                );
+            ELSEIF  rec.SQUATTER_PEND_GUID IS NOT NULL AND rec.SQUATTER_GUID IS NULL THEN
+                INSERT INTO SQ.SQUATTER_MATERIALS (
+                    ID,
+                    SQUATTER_ID, 
+                    MATERIAL_ID, 
+                    SQUATTER_PEND_GUID, 
+                    SQUATTER_VERSION,
+                    CREATED_AT,
+                    UPDATED_AT
+                ) VALUES (
+                    v_guid,
+                    rec.SQUATTERID, 
+                    rec.MATERIAL_ID,
+                    rec.SQUATTER_PEND_GUID,
+                    rec.VERSION,
+                    rec.created_date,
+                    rec.last_edited_date
+                );
+            ELSE
+                v_error_message := 'Missing mandatory join data: SQUATTER_PEND_GUID AND SQUATTER_GUID MATCH WITH A SQUATTERMATIRAL!!';
+                log_error('SQUATTER_MATERIAL', v_error_message, rec.OBJECTID);
+            END IF
         EXCEPTION
             WHEN OTHERS THEN
                 -- Capture the error and log it
