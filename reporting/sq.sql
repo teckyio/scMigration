07e6ffaaa796a2a13eb597cab2eb83a999de5341
leo.yao <LEO.YAO@TECKY.IO>
2024-12-23 18:13:12 +0800

    fix/report

--- reporting/sq.sql
+++ reporting/sq.sql
@@ -10,7 +10,14 @@ SELECT
 FROM SDE_SQ.SQUATTER s
 LEFT JOIN SQ.SQUATTERS t 
     ON s.OBJECTID = t.OBJECT_ID
-WHERE s.SQUATTERID IS NOT NULL;
+WHERE s.SQUATTERID IS NOT NULL
+ORDER BY
+    CASE
+        WHEN s.SQUATTERID = t.SQUATTER_ID THEN 1
+        ELSE 0
+    END,
+    "SOURCE_OBJECTID",
+    "TARGET_OBJECTID";
 
 -- Duplicate Check in Target Table
 -- Ensure no duplicate records exist in the target table.
@@ -22,20 +29,24 @@ HAVING COUNT(*) > 1;
 
 -- Field-Specific Comparison
 -- Compare specific fields between source and target for migrated records.
-SELECT 
-    NVL(s.SURVEYNO, '') || NVL(s.SURVEYNOPREFIX, '') AS "SOURCE_SURVEYNO",
-    NVL(t.SURVEY_NO, '') || NVL(t.SURVEY_NO_PREFIX, '') AS "TARGET_SURVEYNO",
-    CASE 
-        WHEN s.SURVEYNO = t.SURVEY_NO AND s.SURVEYNOPREFIX = t.SURVEY_NO_PREFIX THEN 'MATCH'
-        ELSE 'MISMATCH' 
+SELECT
+    NVL(s.SURVEYNO, 'null') || NVL(s.SURVEY_NOPREFIX, '/null') AS "SOURCE_SURVEYNO",
+    NVL(t.SURVEY_NO, 'null') || NVL(t.SURVEY_NO_PREFIX, '/null') AS "TARGET_SURVEYNO",
+    CASE
+        WHEN NVL(s.SURVEYNO, 'null') = NVL(t.SURVEY_NO, 'null')
+        AND NVL(s.SURVEYNOPREFIX, '/null') = NVL(t.SURVEY_NO_PREFIX, '/null') THEN 'MATCH'
+        ELSE 'MISMATCH'
     END AS "SURVEYNO_MATCH"
 FROM SDE_SQ.SQUATTER s
-JOIN SQ.SQUATTERS t 
+JOIN SQ.SQUATTERS t
     ON s.OBJECTID = t.OBJECT_ID
-ORDER BY 
-    CASE 
-        WHEN s.SURVEYNO = t.SURVEY_NO AND s.SURVEYNOPREFIX = t.SURVEY_NO_PREFIX THEN 1
-        ELSE 0 
-    END, 
-    "SOURCE_SURVEYNO", "TARGET_SURVEYNO";
+ORDER BY
+    CASE
+        WHEN NVL(s.SURVEYNO, 'null') = NVL(t.SURVEY_NO, 'null')
+        AND NVL(s.SURVEYNOPREFIX, '/null') = NVL(t.SURVEY_NO_PREFIX, '/null') THEN 1
+        ELSE 0
+    END,
+    "SOURCE_SURVEYNO",
+    "TARGET_SURVEYNO";
+
 
--- reporting/sq_use_his.sql
+++ reporting/sq_use_his.sql
@@ -2,42 +2,50 @@
 -- Verify that critical fields in the target tables match the source tables for migrated records.
 SELECT 
     sm.OBJECTID AS "SOURCE_OBJECTID",
-    NVL(sm.MATERIALS, '') AS "SOURCE_MATERIAL",
-    NVL(m.NAME, '') AS "TARGET_MATERIAL",
+    NVL(sm.SQUATTERUSE , '') AS "SOURCE_USE",
+    NVL(m.NAME, '') AS "TARGET_USE",
     CASE 
-        WHEN NVL(sm.MATERIALS, '') = NVL(m.NAME, '') THEN 'MATCH'
+        WHEN NVL(sm.SQUATTERUSE , '') = NVL(m.NAME, '') THEN 'MATCH'
         ELSE 'MISMATCH'
-    END AS "MATERIALS_MATCH"
-FROM SDE_SQ.SQUATTERMATERIAL sm
-LEFT JOIN SC.SQUATTER_MATERIALS t 
+    END AS "USE_MATCH"
+FROM SDE_SQ.SQUATTERUSE_HIS sm
+LEFT JOIN SQ.SQUATTER_USE_HIS t 
     ON sm.OBJECTID = t.OBJECT_ID
-LEFT JOIN SQ.MATERIALS m 
-    ON t.MATERIAL_ID = m.ID
+LEFT JOIN SQ.USES m 
+    ON t.USE_ID = m.ID
 ORDER BY 
     CASE 
-        WHEN NVL(sm.MATERIALS, '') = NVL(m.NAME, '') THEN 1
+        WHEN NVL(sm.SQUATTERUSE , '') = NVL(m.NAME, '') THEN 1
         ELSE 0
     END, 
-    sm.OBJECTID, sm.MATERIALS, m.NAME;
+    sm.OBJECTID, sm.SQUATTERUSE , m.NAME;
 
 
 -- Duplicate Records
 -- Ensure there are no duplicate records in the target tables.
 SELECT 
     OBJECT_ID, COUNT(*) 
-FROM SQ.SQUATTER_MATERIALS
+FROM SQ.SQUATTER_USE_HIS su 
 GROUP BY OBJECT_ID
 HAVING COUNT(*) > 1;
 
 -- Referential Integrity Validation
 -- Ensure relationships between foreign key fields (e.g., SQUATTER_GUID, MATERIAL_ID) are maintained.
 SELECT 
+	sm.OBJECTID AS "OBJECTID",
     sm.SQUATTERID AS "SOURCE_SQUATTER_ID",
     t.SQUATTER_ID AS "TARGET_SQUATTER_ID",
     CASE 
-        WHEN sm.SQUATTERID = t.SQUATTER_ID THEN 'MATCH'
+        WHEN NVL(sm.SQUATTERID, '0') = NVL(t.SQUATTER_ID, '0') THEN 'MATCH'
         ELSE 'MISMATCH'
     END AS "RELATION_MATCH"
-FROM SDE_SQ.SQUATTERMATERIAL sm
-LEFT JOIN SQ.SQUATTER_MATERIALS t 
-    ON sm.OBJECTID = t.OBJECT_ID;
+FROM SDE_SQ.SQUATTERUSE_HIS sm
+LEFT JOIN SQ.SQUATTER_USE_HIS t 
+    ON sm.OBJECTID = t.OBJECT_ID
+ORDER BY 
+    CASE 
+        WHEN NVL(sm.SQUATTERID, '0') = NVL(t.SQUATTER_ID, '0') THEN 1
+        ELSE 0
+    END, 
+	"OBJECTID", "SOURCE_SQUATTER_ID", "TARGET_SQUATTER_ID";
+
