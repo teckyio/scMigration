100d0c4fbb9112feac16b94cd4c7299a771d9f9f
leo.yao <LEO.YAO@TECKY.IO>
2025-01-07 11:56:15 +0800

    fix null value

--- reporting/Data Content Validation/full_check_sq_his.sql
+++ reporting/Data Content Validation/full_check_sq_his.sql
@@ -15,13 +15,13 @@ SELECT rec.objectid AS objectid,
 			WHEN rec.SQUATTERID = s.squatter_id THEN ''
 			ELSE 'SQUATTER_ID;'
 		END || CASE
-			WHEN rec.DIMENSION_L = s.dimensions_l THEN ''
+			WHEN NVL(rec.DIMENSION_L, -1) = NVL(s.dimensions_l, -1) THEN ''
 			ELSE 'DIMENSIONS_L;'
 		END || CASE
-			WHEN rec.DIMENSION_B = s.dimensions_b THEN ''
+			WHEN NVL(rec.DIMENSION_B, -1) = NVL(s.dimensions_b, -1) THEN ''
 			ELSE 'DIMENSIONS_B'
 		END || CASE
-			WHEN rec.DIMENSION_H = s.dimensions_h THEN ''
+			WHEN NVL(rec.DIMENSION_H, -1) = NVL(s.dimensions_h, -1) THEN ''
 			ELSE 'DIMENSIONS_H'
 		END || CASE
 			WHEN rec.LOCATION = s.survey_location THEN ''
@@ -51,16 +51,16 @@ SELECT rec.objectid AS objectid,
 			WHEN rec.CISSQUATTERID = s.cis_squatter_id THEN ''
 			ELSE 'CIS_SQUATTER_ID;'
 		END || CASE
-			WHEN rec.BOOKNO = s.book_no THEN ''
+			WHEN NVL(rec.BOOKNO, 'NULL') = NVL(s.book_no, 'NULL') THEN ''
 			ELSE 'BOOK_NO;'
 		END || CASE
-			WHEN rec.SERIALNO = s.serial_no THEN ''
+			WHEN NVL(rec.SERIALNO,'NULL') = NVL(s.serial_no,'NULL') THEN ''
 			ELSE 'SERIAL_NO;'
 		END || CASE
-			WHEN rec.SURVEYNO = s.survey_no THEN ''
+			WHEN NVL(rec.SURVEYNO, 'NULL') = NVL(s.survey_no, 'NULL') THEN ''
 			ELSE 'SURVEY_NO;'
 		END || CASE
-			WHEN rec.FILEREF = s.case_reference THEN ''
+			WHEN NVL(rec.FILEREF, 'NULL') = NVL(s.case_reference, 'NULL') THEN ''
 			ELSE 'CASE_REFERENCE;'
 		END || CASE
 			WHEN NVL(rec.ISSUE, -1) = NVL(s.DATA_PROBLEM, -1) THEN ''
@@ -69,16 +69,16 @@ SELECT rec.objectid AS objectid,
 			WHEN rec.SCOFFICE = s.sc_office THEN ''
 			ELSE 'SC_OFFICE;'
 		END || CASE
-			WHEN rec.SURVEYNOPREFIX = s.survey_no_prefix THEN ''
+			WHEN NVL(rec.SURVEYNOPREFIX, 'NULL') = NVL(s.survey_no_prefix, 'NULL') THEN ''
 			ELSE 'SURVEY_NO_PREFIX;'
 		END || CASE
-			WHEN rec.HASREMARK = s.has_remark THEN ''
+			WHEN NVL(rec.HASREMARK, 'NULL') = NVL(s.has_remark, 'NULL') THEN ''
 			ELSE 'HAS_REMARK;'
 		END || CASE
 			WHEN NVL(rec.HOUSENO, -1) = NVL(s.house_no, -1) THEN ''
 			ELSE 'HOUSE_NO;'
 		END || CASE
-			WHEN rec.DISPLAYNAME = s.display_name THEN ''
+			WHEN NVL(rec.DISPLAYNAME, 'NULL') = NVL(s.display_name, 'NULL') THEN ''
 			ELSE 'DISPLAY_NAME;'
 		END || CASE
 			WHEN rec.BOUNDARYSTATUS = s.CERTAINTY_OF_DIGITIZED_POLYGON THEN ''
@@ -87,7 +87,7 @@ SELECT rec.objectid AS objectid,
 			WHEN rec.DIMENSIONUNIT = s.units THEN ''
 			ELSE 'UNITS;'
 		END || CASE
-			WHEN rec.SERIALNO_EDIT = s.serial_no_edit THEN ''
+			WHEN NVL(rec.SERIALNO_EDIT, 'NULL') = NVL(s.serial_no_edit, 'NULL') THEN ''
 			ELSE 'SERIAL_NO_EDIT;'
 		END || CASE
 			WHEN NVL(
@@ -196,7 +196,7 @@ SELECT rec.objectid AS objectid,
 				rec.CREATED_DATE,
 				TO_DATE('1900-01-01', 'YYYY-MM-DD')
 			) = NVL(
-				s.CREATED_AT,
+				s.created_at,
 				TO_DATE('1900-01-01', 'YYYY-MM-DD')
 			) THEN ''
 			ELSE 'CREATED_DATE;'
@@ -214,209 +214,210 @@ SELECT rec.objectid AS objectid,
 	),
 	CASE
 		WHEN (
-			CASE
-				WHEN rec.OBJECTID = s.OBJECT_ID THEN ''
-				ELSE 'OBJECT_ID;'
-			END || CASE
-				WHEN rec.SQUATTERID = s.squatter_id THEN ''
-				ELSE 'SQUATTER_ID;'
-			END || CASE
-				WHEN rec.DIMENSION_L = s.dimensions_l THEN ''
-				ELSE 'DIMENSIONS_L;'
-			END || CASE
-				WHEN rec.DIMENSION_B = s.dimensions_b THEN ''
-				ELSE 'DIMENSIONS_B'
-			END || CASE
-				WHEN rec.DIMENSION_H = s.dimensions_h THEN ''
-				ELSE 'DIMENSIONS_H'
-			END || CASE
-				WHEN rec.LOCATION = s.survey_location THEN ''
-				ELSE 'SURVEY_LOCATION;'
-			END || CASE
-				WHEN rec.FILENAME = s.file_name THEN ''
-				ELSE 'FILE_NAME;'
-			END || CASE
-				WHEN rec.STATUS = s.status THEN ''
-				ELSE 'STATUS;'
-			END || CASE
-				WHEN rec.SQUATTERDISTRICT = s.district THEN ''
-				ELSE 'DISTRICT;'
-			END || CASE
-				WHEN rec.PLANFILENAME = s.SC_PLAN_NO THEN ''
-				ELSE 'PLAN_FILE_NAME;'
-			END || CASE
-				WHEN rec.CREATED_USER = s.created_user THEN ''
-				ELSE 'CREATED_USER;'
-			END || CASE
-				WHEN rec.LAST_EDITED_USER = s.last_edited_user THEN ''
-				ELSE 'LAST_EDITED_USER;'
-			END || CASE
-				WHEN rec.LAST_EDITED_DATE = s.last_edited_date THEN ''
-				ELSE 'LAST_EDITED_DATE;'
-			END || CASE
-				WHEN rec.CISSQUATTERID = s.cis_squatter_id THEN ''
-				ELSE 'CIS_SQUATTER_ID;'
-			END || CASE
-				WHEN rec.BOOKNO = s.book_no THEN ''
-				ELSE 'BOOK_NO;'
-			END || CASE
-				WHEN rec.SERIALNO = s.serial_no THEN ''
-				ELSE 'SERIAL_NO;'
-			END || CASE
-				WHEN rec.SURVEYNO = s.survey_no THEN ''
-				ELSE 'SURVEY_NO;'
-			END || CASE
-				WHEN rec.FILEREF = s.case_reference THEN ''
-				ELSE 'CASE_REFERENCE;'
-			END || CASE
-				WHEN NVL(rec.ISSUE, -1) = NVL(s.DATA_PROBLEM, -1) THEN ''
-				ELSE 'ISSUE;'
-			END || CASE
-				WHEN rec.SCOFFICE = s.sc_office THEN ''
-				ELSE 'SC_OFFICE;'
-			END || CASE
-				WHEN rec.SURVEYNOPREFIX = s.survey_no_prefix THEN ''
-				ELSE 'SURVEY_NO_PREFIX;'
-			END || CASE
-				WHEN rec.HASREMARK = s.has_remark THEN ''
-				ELSE 'HAS_REMARK;'
-			END || CASE
-				WHEN NVL(rec.HOUSENO, -1) = NVL(s.house_no, -1) THEN ''
-				ELSE 'HOUSE_NO;'
-			END || CASE
-				WHEN rec.DISPLAYNAME = s.display_name THEN ''
-				ELSE 'DISPLAY_NAME;'
-			END || CASE
-				WHEN rec.BOUNDARYSTATUS = s.CERTAINTY_OF_DIGITIZED_POLYGON THEN ''
-				ELSE 'BOUNDARY_STATUS;'
-			END || CASE
-				WHEN rec.DIMENSIONUNIT = s.units THEN ''
-				ELSE 'UNITS;'
-			END || CASE
-				WHEN rec.SERIALNO_EDIT = s.serial_no_edit THEN ''
-				ELSE 'SERIAL_NO_EDIT;'
-			END || CASE
-				WHEN NVL(
-					rec.RECORDDATE_EDIT,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.HD161FORM_RECORD_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'RECORD_DATE_EDIT;'
-			END || CASE
-				WHEN NVL(rec.DELETE_REASON, -1) = NVL(s.DELETE_REASON, -1) THEN ''
-				ELSE 'DELETE_REASON;'
-			END || CASE
-				WHEN NVL(
-					rec.DELETE_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.delete_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'DELETE_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.REINSTATE_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.re_instate_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'RE_INSTATE_DATE;'
-			END || CASE
-				WHEN rec.VERSION = s.version THEN ''
-				ELSE 'VERSION;'
-			END || CASE
-				WHEN NVL(rec.SURVEYRECORD_1982, 'NULL') = NVL(s.survey_record1982, 'NULL') THEN ''
-				ELSE 'SURVEY_RECORD_1982;'
-			END || CASE
-				WHEN NVL(
-					rec.APPROVED_CREATION_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.approved_creation_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'APPROVED_CREATION_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.APPROVED_DELETE_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.approved_delete_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'APPROVED_DELETE_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.APPROVED_REINSTATE_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.approved_reinstate_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'APPROVED_REINSTATE_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.APPROVED_AMEND_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.approved_amendment_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'APPROVED_AMENDMENT_DATE;'
-			END || CASE
-				WHEN NVL(rec.JOBNO, 'NULL') = NVL(s.job_no, 'NULL') THEN ''
-				ELSE 'JOB_NO;'
-			END || CASE
-				WHEN NVL(rec.CLEARANCE_NO, 'NULL') = NVL(s.clearance_no, 'NULL') THEN ''
-				ELSE 'CLEARANCE_NO;'
-			END || CASE
-				WHEN NVL(
-					rec.AMEND_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.amend_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'AMEND_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.CASEFILE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(s.case_file, TO_DATE('1900-01-01', 'YYYY-MM-DD')) THEN ''
-				ELSE 'CASE_FILE;'
-			END || CASE
-				WHEN NVL(
-					rec.RECORDDATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.record_date,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'RECORD_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.CREATED_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.CREATED_AT,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'CREATED_DATE;'
-			END || CASE
-				WHEN NVL(
-					rec.LAST_EDITED_DATE,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) = NVL(
-					s.updated_at,
-					TO_DATE('1900-01-01', 'YYYY-MM-DD')
-				) THEN ''
-				ELSE 'UPDATED_AT;'
-			END
-		) IS NULL THEN 1
+		CASE
+			WHEN rec.OBJECTID = s.OBJECT_ID THEN ''
+			ELSE 'OBJECT_ID;'
+		END || CASE
+			WHEN rec.SQUATTERID = s.squatter_id THEN ''
+			ELSE 'SQUATTER_ID;'
+		END || CASE
+			WHEN NVL(rec.DIMENSION_L, -1) = NVL(s.dimensions_l, -1) THEN ''
+			ELSE 'DIMENSIONS_L;'
+		END || CASE
+			WHEN NVL(rec.DIMENSION_B, -1) = NVL(s.dimensions_b, -1) THEN ''
+			ELSE 'DIMENSIONS_B'
+		END || CASE
+			WHEN NVL(rec.DIMENSION_H, -1) = NVL(s.dimensions_h, -1) THEN ''
+			ELSE 'DIMENSIONS_H'
+		END || CASE
+			WHEN rec.LOCATION = s.survey_location THEN ''
+			ELSE 'SURVEY_LOCATION;'
+		END || CASE
+			WHEN rec.FILENAME = s.file_name THEN ''
+			ELSE 'FILE_NAME;'
+		END || CASE
+			WHEN rec.STATUS = s.status THEN ''
+			ELSE 'STATUS;'
+		END || CASE
+			WHEN rec.SQUATTERDISTRICT = s.district THEN ''
+			ELSE 'DISTRICT;'
+		END || CASE
+			WHEN rec.PLANFILENAME = s.SC_PLAN_NO THEN ''
+			ELSE 'PLAN_FILE_NAME;'
+		END || CASE
+			WHEN rec.CREATED_USER = s.created_user THEN ''
+			ELSE 'CREATED_USER;'
+		END || CASE
+			WHEN rec.LAST_EDITED_USER = s.last_edited_user THEN ''
+			ELSE 'LAST_EDITED_USER;'
+		END || CASE
+			WHEN rec.LAST_EDITED_DATE = s.last_edited_date THEN ''
+			ELSE 'LAST_EDITED_DATE;'
+		END || CASE
+			WHEN rec.CISSQUATTERID = s.cis_squatter_id THEN ''
+			ELSE 'CIS_SQUATTER_ID;'
+		END || CASE
+			WHEN NVL(rec.BOOKNO, 'NULL') = NVL(s.book_no, 'NULL') THEN ''
+			ELSE 'BOOK_NO;'
+		END || CASE
+			WHEN NVL(rec.SERIALNO,'NULL') = NVL(s.serial_no,'NULL') THEN ''
+			ELSE 'SERIAL_NO;'
+		END || CASE
+			WHEN NVL(rec.SURVEYNO, 'NULL') = NVL(s.survey_no, 'NULL') THEN ''
+			ELSE 'SURVEY_NO;'
+		END || CASE
+			WHEN NVL(rec.FILEREF, 'NULL') = NVL(s.case_reference, 'NULL') THEN ''
+			ELSE 'CASE_REFERENCE;'
+		END || CASE
+			WHEN NVL(rec.ISSUE, -1) = NVL(s.DATA_PROBLEM, -1) THEN ''
+			ELSE 'ISSUE;'
+		END || CASE
+			WHEN rec.SCOFFICE = s.sc_office THEN ''
+			ELSE 'SC_OFFICE;'
+		END || CASE
+			WHEN NVL(rec.SURVEYNOPREFIX, 'NULL') = NVL(s.survey_no_prefix, 'NULL') THEN ''
+			ELSE 'SURVEY_NO_PREFIX;'
+		END || CASE
+			WHEN NVL(rec.HASREMARK, 'NULL') = NVL(s.has_remark, 'NULL') THEN ''
+			ELSE 'HAS_REMARK;'
+		END || CASE
+			WHEN NVL(rec.HOUSENO, -1) = NVL(s.house_no, -1) THEN ''
+			ELSE 'HOUSE_NO;'
+		END || CASE
+			WHEN NVL(rec.DISPLAYNAME, 'NULL') = NVL(s.display_name, 'NULL') THEN ''
+			ELSE 'DISPLAY_NAME;'
+		END || CASE
+			WHEN rec.BOUNDARYSTATUS = s.CERTAINTY_OF_DIGITIZED_POLYGON THEN ''
+			ELSE 'BOUNDARY_STATUS;'
+		END || CASE
+			WHEN rec.DIMENSIONUNIT = s.units THEN ''
+			ELSE 'UNITS;'
+		END || CASE
+			WHEN NVL(rec.SERIALNO_EDIT, 'NULL') = NVL(s.serial_no_edit, 'NULL') THEN ''
+			ELSE 'SERIAL_NO_EDIT;'
+		END || CASE
+			WHEN NVL(
+				rec.RECORDDATE_EDIT,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.HD161FORM_RECORD_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'RECORD_DATE_EDIT;'
+		END || CASE
+			WHEN NVL(rec.DELETE_REASON, -1) = NVL(s.DELETE_REASON, -1) THEN ''
+			ELSE 'DELETE_REASON;'
+		END || CASE
+			WHEN NVL(
+				rec.DELETE_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.delete_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'DELETE_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.REINSTATE_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.re_instate_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'RE_INSTATE_DATE;'
+		END || CASE
+			WHEN rec.VERSION = s.version THEN ''
+			ELSE 'VERSION;'
+		END || CASE
+			WHEN NVL(rec.SURVEYRECORD_1982, 'NULL') = NVL(s.survey_record1982, 'NULL') THEN ''
+			ELSE 'SURVEY_RECORD_1982;'
+		END || CASE
+			WHEN NVL(
+				rec.APPROVED_CREATION_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.approved_creation_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'APPROVED_CREATION_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.APPROVED_DELETE_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.approved_delete_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'APPROVED_DELETE_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.APPROVED_REINSTATE_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.approved_reinstate_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'APPROVED_REINSTATE_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.APPROVED_AMEND_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.approved_amendment_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'APPROVED_AMENDMENT_DATE;'
+		END || CASE
+			WHEN NVL(rec.JOBNO, 'NULL') = NVL(s.job_no, 'NULL') THEN ''
+			ELSE 'JOB_NO;'
+		END || CASE
+			WHEN NVL(rec.CLEARANCE_NO, 'NULL') = NVL(s.clearance_no, 'NULL') THEN ''
+			ELSE 'CLEARANCE_NO;'
+		END || CASE
+			WHEN NVL(
+				rec.AMEND_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.amend_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'AMEND_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.CASEFILE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(s.case_file, TO_DATE('1900-01-01', 'YYYY-MM-DD')) THEN ''
+			ELSE 'CASE_FILE;'
+		END || CASE
+			WHEN NVL(
+				rec.RECORDDATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.record_date,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'RECORD_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.CREATED_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.created_at,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'CREATED_DATE;'
+		END || CASE
+			WHEN NVL(
+				rec.LAST_EDITED_DATE,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) = NVL(
+				s.updated_at,
+				TO_DATE('1900-01-01', 'YYYY-MM-DD')
+			) THEN ''
+			ELSE 'UPDATED_AT;'
+		END,
+		'; '
+	) IS NULL THEN 1
 		ELSE 0
 	END AS is_valid
 FROM SDE_SQ.SQUATTER_HIS rec
